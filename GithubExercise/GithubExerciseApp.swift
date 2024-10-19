9342cc0ff7274c441ddaa7dae22a161ae24d7887
Steve Kite <skite1@hotmail.com>
2024-10-19 10:24:58 -0700

    Initial Commit

---
+++ GithubExercise/GithubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubExerciseApp.swift
+//  GithubExercise
+//
+//  Created by Steve on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
