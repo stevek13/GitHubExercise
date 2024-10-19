f9dcfc6fad6e49d719fe06e77ac0c180cbcbde07
Steve Kite <skite1@hotmail.com>
2024-10-19 10:29:35 -0700

    Version 1.0 - Red

--- GithubExercise/ContentView.swift
+++ GithubExercise/ContentView.swift
@@ -10,11 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+            Text("Version 1.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
         }
+        .foregroundStyle(.red)
         .padding()
     }
 }
